243a37a17dfb693fca24b463c982a45953126d58
jcszuster-cmd <jcszuster@gmail.com>
2025-10-07 22:10:08 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,3 +5,8 @@ st.write("alo mundo")
 nome = st.text_input("digite o seu nome:")
 if nome:
     st.write(nome.upper())
+
+curso = st.text_input("qual curso você faz?")
+if curso = direito
+    st.write("Que interessante!")
+else st.write("Que triste :(")
