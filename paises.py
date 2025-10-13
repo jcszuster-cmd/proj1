be8e970676d8609f25ecc6a7f3faff3b92dc7404
jcszuster-cmd <jcszuster@gmail.com>
2025-10-13 15:08:36 -0300

    Update paises.py

--- paises.py
+++ paises.py
@@ -4,4 +4,11 @@ import plotly.express as px
 
 dataset = pd.read_csv ('https://www.irdx.com.br/media/uploads/paises.csv')
 
+fig = px.choropleth(dataset,
+                    locations=dataset['iso3'],
+                    color=dataset['nome'],
+                    hover_name=dataset['nome'])
+fig.update_layout(title='Mapa coroplético dos paises',
+                  geo_scope='world')
+fig.show()
 
