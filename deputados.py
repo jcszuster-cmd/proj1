26aec3fba8858cd6e2336cad86958a362dd79ecf
jcszuster-cmd <jcszuster@gmail.com>
2025-10-14 11:43:15 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('https://ss.cursos.fgv.br/d2l/le/content/179108/viewContent/2436430/View')
 
 fig, ax = plt.subplots()
-ax.bar(df['Categoria'], df['Valor'])
-ax.set_xlabel('Categoria')
+ax.bar(df['Partido'], df['Valor'])
+ax.set_xlabel('Partido')
 ax.set_ylabel('Valor')
 ax.set_title('Gráfico de Barras')
 
