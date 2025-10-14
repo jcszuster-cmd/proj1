501aee25d945a8e34349513c6b1825b6dce37eeb
jcszuster-cmd <jcszuster@gmail.com>
2025-10-14 11:45:42 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('https://ss.cursos.fgv.br/d2l/le/content/179108/viewContent/2436430/View')
+df = pd.read_csv('deputados_2022 (1).csv')
 
 fig, ax = plt.subplots()
 ax.bar(df['Partido'], df['Valor'])
